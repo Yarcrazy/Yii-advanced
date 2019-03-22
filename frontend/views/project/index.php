13d8cd81462c4551749758d5cf2e8776e2532204
Yarcrazy <yacrazy@yandex.ru>
2019-03-23 00:04:11 +0700

    поправил отображения полей во вьюхе index project фронта

--- frontend/views/project/index.php
+++ frontend/views/project/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\search\ProjectSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -12,38 +13,49 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="project-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode($this->title) ?></h1>
 
-    <?php Pjax::begin(); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+  <?php Pjax::begin(); ?>
+  <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+  <?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+      ['class' => 'yii\grid\SerialColumn'],
 
-            'title',
-            [
-            	'attribute' => \common\models\Project::RELATION_PROJECT_USERS.'.role',
-							'value' => function(\common\models\Project $model) {
-                return join(', ', Yii::$app->projectService->getRoles($model, Yii::$app->user->identity));
-							},
-							'format' => 'html',
-						],
-            'description:ntext',
-            'active',
-            'creator_id',
+      'title',
+      [
+        'attribute' => \common\models\Project::RELATION_PROJECT_USERS . '.role',
+        'value' => function (\common\models\Project $model) {
+          return join(', ', Yii::$app->projectService->getRoles($model, Yii::$app->user->identity));
+        },
+        'format' => 'html',
+      ],
+      'description:ntext',
+      [
+        'attribute' => 'active',
+        'value' => function (\common\models\Project $model) {
+          return \common\models\Project::STATUS_LABELS[$model->active];
+        }
+      ],
+      [
+        'attribute' => 'creator_id',
+				'label' => 'Creator',
+        'value' => function (\common\models\Project $model) {
+          return $model->creator->username;
+        }
+      ],
 
-            //'updater_id',
-            //'created_at',
-            //'updated_at',
+      //'updater_id',
+      //'created_at',
+      //'updated_at',
 
-            ['class' => 'yii\grid\ActionColumn',
-							'template' => '{view} {delete}'],
-        ],
-    ]); ?>
+      ['class' => 'yii\grid\ActionColumn',
+        'template' => '{view} {delete}'],
+    ],
+  ]); ?>
 
-    <?php Pjax::end(); ?>
+  <?php Pjax::end(); ?>
 
 </div>
